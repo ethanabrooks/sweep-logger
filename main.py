8ebda6b369c6ab4dbcf127607d0775bcd4d1e1ef
Ethan Brooks <ethanabrooks@gmail.com>
2021-07-25 12:00:39 -0400

    update run_logger and change to use graphql_entpoint and take out hasura_secret

--- main.py
+++ main.py
@@ -8,25 +8,21 @@ from typing import Optional
 import yaml
 from redis import Redis
 
+from sweep_logger import HasuraLogger
 from sweep_logger.params import ParamChoice, SweepMethod
 from sweep_logger.reproducibility_info import get_reproducibility_info
-from sweep_logger.logger import get_logger
-
-
-def load_config(config: Path) -> dict:
-    with config.open() as f:
-        return yaml.load(f, Loader=yaml.FullLoader)
 
 
 def run(
     config: Path,
     log_level: str,
+    method: str,
     name: Optional[str],
     project: Optional[str],
-    logger: str,
-    method: str,
+    graphql_endpoint: str,
 ) -> int:
-    config = load_config(config)
+    with config.open() as f:
+        config = yaml.load(f, yaml.FullLoader)
     logging.getLogger().setLevel(log_level)
     metadata = dict(
         config=config,
@@ -36,7 +32,7 @@ def run(
         metadata.update(name=name)
     if project is not None:
         metadata.update(project=project)
-    with get_logger(logger) as logger:
+    with HasuraLogger(graphql_endpoint=graphql_endpoint) as logger:
         if isinstance(config, list):
             config = {"": config}
         choices = [ParamChoice(k, v) for k, v in config.items()]
@@ -77,19 +73,6 @@ def main():
         type=Path,
         default=Path("config.yml"),
     )
-    parser.add_argument(
-        "--name", "-n", help="name of sweep (logged in metadata)", default=None
-    )
-    parser.add_argument(
-        "--project", "-p", help="name of project (logged in metadata)", default=None
-    )
-    parser.add_argument(
-        "--logger",
-        "-l",
-        help="type of logger",
-        choices=["hasura", "jsonlines"],
-        default="hasura",
-    )
     parser.add_argument("--log-level", "-ll", choices=log_levels, default="INFO")
     parser.add_argument(
         "--method",
@@ -98,6 +81,13 @@ def main():
         default="random",
         help="whether to perform grid-search on parameters in config.yml or randomly sample",
     )
+    parser.add_argument(
+        "--name", "-n", help="name of sweep (logged in metadata)", default=None
+    )
+    parser.add_argument(
+        "--project", "-p", help="name of project (logged in metadata)", default=None
+    )
+    parser.add_argument("--graphql-endpoint", "-g", help="Endpoint to use for hasura")
     parser.set_defaults(func=run)
     subparsers = parser.add_subparsers()
     redis_parser = subparsers.add_parser("redis")
--- sweep_logger/execute_sweep.py
+++ sweep_logger/execute_sweep.py
@@ -2,13 +2,12 @@ import argparse
 import os
 import subprocess
 import time
-from typing import Optional
 
 from redis import Redis
 from run_logger import Client
 
 
-def execute_sweep(hasura_uri: str, hasura_secret: Optional[str], command: str):
+def execute_sweep(graphql_endpoint: str, command: str):
     redis = Redis(host="redis")
     rank = redis.decr("rank-counter")
     print("rank ==", rank)
@@ -20,7 +19,7 @@ def execute_sweep(hasura_uri: str, hasura_secret: Optional[str], command: str):
     env = os.environ.copy()
     env["CUDA_VISIBLE_DEVICES"] = str(rank)
 
-    client = Client(hasura_uri=hasura_uri, hasura_secret=hasura_secret)
+    client = Client(graphql_endpoint)
 
     def keep_running():
         data = client.execute(
@@ -53,6 +52,7 @@ if __name__ == "__main__":
         required=True,
         help="command execute. `sweep_id` will be given to command as in `script sweep_id`.",
     )
-    PARSER.add_argument("--hasura-uri", required=True, help="Graphql endpoint")
-    PARSER.add_argument("--hasura-secret")
+    PARSER.add_argument(
+        "--graphql-endpoint", required=True, help="Endpoint to use for Hasura"
+    )
     execute_sweep(**vars(PARSER.parse_args()))
